655d7f4d39ef9a09dc8028d11abe2740e05fe567
Bishop O'Connell <bishopoconnell1@gmail.com>
2020-05-21 20:45:09 -0400

    added make money back calculation

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main() {
 
-    int turnips_bought {0};
+    int turnips_bought {6000};
     int current_price_per_turnip {0};
-    int initial_price_per_turnip {0};
+    int initial_price_per_turnip {94};
 
-    cout << "How much was the cost per turnip: " << endl;
-    cin >> initial_price_per_turnip;
-    cout << "How turnips did you buy: ";
-    cin >> turnips_bought;
+    // cout << "How much was the cost per turnip: " << endl;
+    // cin >> initial_price_per_turnip;
+    // cout << "How turnips did you buy: ";
+    // cin >> turnips_bought;
 
     cout << "What is the current price: ";
     cin >> current_price_per_turnip;
@@ -20,8 +20,13 @@ int main() {
 
     int current_total = current_price_per_turnip * turnips_bought;
 
-    cout << "Your profit is: " << current_total - initial_cost << endl;
-    cout << "You can do better" << endl;
+    int final_profit = current_total - initial_cost;
+
+    int make_initial_cost_back = (initial_cost)/ current_price_per_turnip;
+
+    cout << "Your profit is: " << final_profit << endl;
+    cout << "To make moeny back and wait for stalk price increase, sell: "
+         << make_initial_cost_back << " turnips." << endl;
 
 
     return 0;
